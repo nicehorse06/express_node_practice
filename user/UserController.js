e0eb2f985bc72e6d102579cdd7c278fc49e50be8
jimmyma <jimmyma@croxlink.com>
2018-07-18 13:39:35 +0800

    move all user to

--- user/UserController.js
+++ user/UserController.js
@@ -13,10 +13,11 @@ router.post('/', (req, res) => {
     User.create({
         name: req.body.name,
         email: req.body.email,
-        passqord: req.body.passqord
+        password: req.body.password
     },(err, user) => {
         // callback function response an error value and a success value.
         if (err) return res.status(500).send("There was a problem adding the information to the database.");
+        console.log(user)
         res.status(200).send(user);
     })
 })
@@ -24,6 +25,7 @@ router.post('/', (req, res) => {
 router.get('/', (req, res) => {
     User.find({}, (err, users) => {
         if (err) return res.status(500).send("There was a problem finding the users.");
+        console.log(users)
         res.status(200).send(users);
     })
 })
