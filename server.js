173934392f062545e55f5212fca7f20ac7ba3cd2
jimmyma <jimmyma@croxlink.com>
2018-07-27 14:09:19 +0800

    add crawler to cnnode.com

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const utility = require('utility');
 const app = require('./app')
 const port = process.env.PORT || 3000
+// 引入用來轉 md5 加密
+const utility = require('utility');
+// 用來發 http 請求的套件
+const superagent = require('superagent');
+// node 版的 jquery
+const cheerio = require('cheerio');
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}`)
@@ -22,4 +27,26 @@ app.get('/encode', (req, res)=>{
     let md5_value = utility.md5(q)
     
     res.send(`${q} 的md5加密結果為: ${md5_value}`)
+})
+
+// 用 superagent 和 cheerio 去抓取網頁內容
+app.get('/crawler', (req, res, next)=>{
+    // 指定去抓此網站的內容
+    superagent.get('https://cnodejs.org/').end((err, sres) => {
+        if(err){
+            return next(err)
+        }
+        // sres.text存有get到的HTML，再用 cheerio.load() 載入
+        // 之後就可以用 jquery 化的方法去處理這個 HTML
+        let $ = cheerio.load(sres.text)
+        let items = []
+        $('#topic_list .topic_title').each(function (idx, element) {
+            var $element = $(element);
+            items.push({
+              title: $element.attr('title'),
+              href: $element.attr('href')
+            });
+        });
+        res.send(items);
+    })
 })
